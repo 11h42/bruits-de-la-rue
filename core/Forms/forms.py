a8759e8a15208394ac2dea809d4161f82061690d
Antoine Briand <abriand@akema.fr>
2014-08-20 17:49:07 +0200

    bidCategory is a ChoiceField with choices loaded from DB. Add CleanPhoto Validator to force user to do not upload photo bigger than 512x512px

--- core/Forms/forms.py
+++ core/Forms/forms.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.core.exceptions import ValidationError
+from PIL import Image
+
 from core.models import Bid, BidCategories
 
 
@@ -9,11 +12,13 @@ class BidForm(forms.ModelForm):
         fields = ['name', 'type', 'begin', 'end', 'status', 'quantity', 'real_author',
                   'emergency_level',
                   'recurrence', 'description', 'bidCategory', 'photo', 'quantity_type', 'adress1', 'adress2', 'zipcode',
-                  'town', 'country']
+                  'town', 'country', 'photo']
 
     def __init__(self, *args, **kwargs):
         super(BidForm, self).__init__(*args, **kwargs)
-        self.fields['bidCategory'].choices = [(Category.id, Category.bid_category_name) for Category in BidCategories.objects.all()]
+        self.fields['bidCategory'].choices = [(Category.id, Category.bid_category_name) for Category in
+                                              BidCategories.objects.all()]
+
 
     TYPES = (('Offre', 'Offre', ), ('Demande', 'Demande', ))
     type = forms.ChoiceField(widget=forms.Select, choices=TYPES)
@@ -26,3 +31,10 @@ class BidForm(forms.ModelForm):
 
     TYPES_EMERGENCIES = (('URGENT', 'URGENT', ), ('Normal', 'Normal', ), ('Faible', 'Faible', ))
     emergency_level = forms.ChoiceField(widget=forms.Select, choices=TYPES_EMERGENCIES)
+
+    def clean_photo(self):
+        photo = self.cleaned_data.get('photo', False)
+        if photo:
+            if Image.open(photo).size > (512, 512):
+                raise ValidationError("Votre image est trop grande (Dimensions max : 512x512 pixels)")
+            return photo
