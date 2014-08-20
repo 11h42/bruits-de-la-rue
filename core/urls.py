c47ddd0416b10a8cd5f3aa3be3acc30407873fd3
Antoine Briand <abriand@akema.fr>
2014-08-20 18:00:27 +0200

    Add ^ at the start of the urls. Add a trick to test upload (Must be replaced)

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from core import views
 
 admin.autodiscover()
@@ -9,9 +10,9 @@ urlpatterns = patterns(
     '',
     url(r'^login/$', views.handle_login, name='login'),
     url(r'^$', views.index, name='index'),
-    url(r'annonce/(?P<pk>\d+)/$', views.bid_handler, name="bid-details"),
-    url(r'annonce/liste/$', views.BidList.as_view(), name="bid-list"),
-    url(r'annonce/creer/$', views.BidCreate.as_view(), name='create-bid'),
-    url(r'annonce/(?P<pk>[0-9]+)/update/$', views.BidUpdate.as_view(), name='offer_update'),
-    url(r'annonce/(?P<pk>[0-9]+)/delete/$', views.BidDelete.as_view(), name='offer_delete'),
-)
+    url(r'^annonce/(?P<pk>\d+)/$', views.bid_handler, name="bid-details"),
+    url(r'^annonce/liste/$', views.BidList.as_view(), name="bid-list"),
+    url(r'^annonce/creer/$', views.BidCreate.as_view(), name='create-bid'),
+    url(r'^annonce/(?P<pk>[0-9]+)/update/$', views.BidUpdate.as_view(), name='offer_update'),
+    url(r'^annonce/(?P<pk>[0-9]+)/delete/$', views.BidDelete.as_view(), name='offer_delete'),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
