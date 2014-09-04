504e7f54c4daedd1338ed731cc3cbd76af01e986
Antoine Briand <antoine.briand@viacesi.fr>
2014-09-04 17:33:09 +0200

    add test user

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -1,3 +1,4 @@
 from test_bids import *
 from test_bids_categories import *
 from test_validators import *
+from test_users import *
---
+++ api/tests/test_users.py
@@ -0,0 +1,22 @@
+import json
+from django.test.testcases import TestCase
+
+from core.tests import factories
+
+
+class UsersTest(TestCase):
+    def setUp(self):
+        self.address = factories.AddressFactory()
+        self.address2 = factories.AddressFactory(title='Maison')
+        self.user = factories.UserFactory.create(username='jdupont', address=[self.address])
+        self.client.login(username=self.user.username, password="password")
+
+    def test_get_current_user_username(self):
+        response = self.client.get('/api/users/current/')
+        self.assertEquals(response.content, self.user.username)
+
+    def test_get_current_user_address(self):
+        response = self.client.get('/api/users/current/address')
+        address_returned = json.loads(response.content)
+        self.assertEquals(200, response.status_code)
+        self.assertEquals(address_returned, {})
