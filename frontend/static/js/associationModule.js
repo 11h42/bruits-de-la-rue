ca7ac26ce28b221bc1d3ac97ab04f4f3010e261c
Antoine Briand <antoine.briand@viacesi.fr>
2014-09-18 18:51:27 +0200

    start get association

--- frontend/static/js/associationModule.js
+++ frontend/static/js/associationModule.js
@@ -24,12 +24,23 @@ associationsModule.controller('associationsController', function ($scope, $http)
         return Math.ceil($scope.associations.length / $scope.pageSize);
     };
 
-    $http.get('/api/associations/').
-        success(function (data) {
-            $scope.associations = data.associations;
-        }).error(function (data, status, headers, config) {
-            $scope.errorMessage = "Veuillez nous excuser, notre site rencontre des difficultés techniques. Nous vous invitions à réessayer dans quelques minutes.";
-        });
+    var url = $location.absUrl();
+    var url_split = url.split('/');
+    var indexOfId = url_split.indexOf('associations') + 1;
+    var associationId = url_split[indexOfId];
+    var isInt = /^\d+$/;
+
+    if (isInt.test(associationId)) {
+        $http.get('/api/associations/').
+            success(function (data) {
+                $scope.associations = data.associations;
+            }).error(function (data, status, headers, config) {
+                $scope.errorMessage = "Veuillez nous excuser, notre site rencontre des difficultés techniques. Nous vous invitions à réessayer dans quelques minutes.";
+            });
+    } else {
+// get association
+    }
+
     $scope.showAssociation = function (association_id) {
         window.location = '/associations/' + association_id + '/';
     };
