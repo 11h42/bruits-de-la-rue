6cae2434f67d5a0ecbcd751eb90ffad62482d14d
Guillaume Vincent <guillaume@oslab.fr>
2016-02-11 20:39:22 +0100

    add superuser field in admin

--- core/admin.py
+++ core/admin.py
@@ -17,7 +17,7 @@ class UserCreationForm(forms.ModelForm):
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'addresses')
+        fields = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'addresses')
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
@@ -60,7 +60,7 @@ class CustomUserAdmin(UserAdmin):
 
     list_filter = ()
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'username', 'is_staff', 'addresses')}),
+        (None, {'fields': ('email', 'password', 'username', 'is_staff', 'is_superuser', 'addresses')}),
         ('Informations personnelles', {'fields': ('first_name', 'last_name')}),
     )
     add_fieldsets = (
