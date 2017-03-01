50c0f3f108b4099284bbe3239aa6d1fcbd7f54f4
Guillaume Vincent <gvincent@redhat.com>
2017-03-01 17:09:03 +0100

    no prompt collectstatic

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ source env/bin/activate
 python -m pip install -U pip
 python -m pip install -r requirements.txt
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
